66f1adc1c97fcebe0639ad25823f6689b4d8b211
Faiyaz Ahmed <faiyaz@nextventures.io>
2026-06-21 00:24:36 +0600

    Update translation settings in layout.js
    Removed 'notranslate' attributes from HTML and body tags.

--- app/layout.js
+++ app/layout.js
@@ -4,15 +4,12 @@ import AppShellClient from "./components/AppShellClient";
 export const metadata = {
   title: "AI Auditor & Insights Platform",
   description: "Internal audit system",
-  other: {
-    google: "notranslate",
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="notranslate" translate="no">
-      <body className="notranslate" translate="no">
+    <html lang="en" translate="yes">
+      <body translate="yes">
         <AppShellClient>{children}</AppShellClient>
       </body>
     </html>
